5183b5655b3fefbe127fe375e163eaa73783aacf
CrowKing63 <choiuitaek@gmail.com>
2025-09-22 10:27:56 +0900

    Initial Commit

---
+++ ChronicaeTests/ChronicaeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChronicaeTests.swift
+//  ChronicaeTests
+//
+//  Created by 최의택 on 9/22/25.
+//
+
+import Testing
+@testable import Chronicae
+
+struct ChronicaeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
